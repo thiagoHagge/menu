57a4d2b566b7afab7b63957ac24d442d86d531e6
thiagoHagge <thiagohagge09@gmail.com>
2021-09-27 22:13:35 -0300

    try sqlite

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,7 +5,12 @@ export default function Login() {
     return (
         <div className={styles.container}>
             <Head>
-
+                <link
+                    rel="preload"
+                    href="/fonts/RebelHero/Rebel-Hero.otf"
+                    as="font"
+                    crossOrigin=""
+                />
             </Head>
             <div className={styles.main}>
                 <input className={styles.formControl} placeholder="User" />
